28de2f9b0ebe76a59ab777d422707d8f490e5451
JamesGlendinning <jtglendinning100@student.hbuhsd.edu>
2018-01-20 14:28:17 -0800

    created Climber

--- 2018 Robot project/src/mechanisms/Climber.java
+++ 2018 Robot project/src/mechanisms/Climber.java
@@ -1,5 +1,28 @@
 package mechanisms;
 
+import org.usfirst.frc.team4276.robot.Robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import utilities.Xbox;
+
 public class Climber {
+	VictorSPX climberMotor;
+	final double MOTOR_POWER = 1;
+
+	public Climber(int canPort) {
+		climberMotor = new VictorSPX(canPort);
+
+	}
+
+	public void performMainProcessing() {
+		if (Robot.xboxController.getPOV(Xbox.DPad) == Xbox.POVup) {
+			climberMotor.set(ControlMode.PercentOutput, MOTOR_POWER);
+
+		} else {
+			climberMotor.set(ControlMode.PercentOutput, 0);
+		}
+
+	}
 
 }
