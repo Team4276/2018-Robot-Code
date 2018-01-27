591077c162a63fb10ea108bac3ae0c9158824328
bistroboy <maxjaggli@gmail.com>
2018-01-27 11:50:01 -0800

    added autonomous functions to Manipulator.java

--- 2018 Robot project/src/org/usfirst/frc/team4276/systems/Manipulator.java
+++ 2018 Robot project/src/org/usfirst/frc/team4276/systems/Manipulator.java
@@ -7,7 +7,6 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Manipulator {
-
 	Solenoid solenoid;
 	Toggler rtToggler;
 	final double TRIGGER_THRESHOLD = -0.5;
@@ -15,23 +14,41 @@ public class Manipulator {
 	boolean solenoidIsOpen;
 
 	public Manipulator(int canPort) {
-
 		solenoid = new Solenoid(canPort);
 		rtToggler = new Toggler(Xbox.RT);
 	}
 
-	public void performMainProcessing() {
-
-		rtToggler.updateMechanismState(TRIGGER_THRESHOLD);
-
-		manipulatorCommandedOpen = rtToggler.getMechanismState();
-
-		solenoid.set(manipulatorCommandedOpen);
-
+	public void openManipulator() {
+		// get current solenoid status
 		solenoidIsOpen = solenoid.get();
 
-		SmartDashboard.putBoolean("manipulator open", solenoidIsOpen);
-
+		// if solenoid is not(!) open, then open it
+		if (!solenoidIsOpen) {
+			solenoid.set(true);
+		}
 	}
 
+	public void closeManipulator() {
+		// get current solenoid status
+		solenoidIsOpen = solenoid.get();
+
+		// if solenoid is open, then close it
+		if (solenoidIsOpen) {
+			solenoid.set(false);
+		}
+	}
+
+	public void performMainProcessing() {
+		// get desired manipulator position based on right trigger toggler
+		rtToggler.updateMechanismState(TRIGGER_THRESHOLD);
+		manipulatorCommandedOpen = rtToggler.getMechanismState();
+
+		// open or close solenoid depending on desired manipulator position
+		solenoid.set(manipulatorCommandedOpen);
+	}
+
+	public void updateTelemetry() {
+		solenoidIsOpen = solenoid.get();
+		SmartDashboard.putBoolean("manipulator open", solenoidIsOpen);
+	}
 }
