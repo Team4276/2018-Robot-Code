efcd7d66ccfc04e137f349bbcc479297ad4df0fd
AndyC <andrew.cappon@gmail.com>
2018-01-27 11:10:26 -0800

    Two cameras, 2.3 Mbps

--- 2018 Robot project/src/org/usfirst/frc/team4276/systems/Cameras.java
+++ 2018 Robot project/src/org/usfirst/frc/team4276/systems/Cameras.java
@@ -10,12 +10,12 @@ public class Cameras {
 
 	private final int MAIN_RES_X = 320;
 	private final int MAIN_RES_Y = 240;
-	private final int MAIN_FPS = 30;
+	private final int MAIN_FPS = 15;
 	private final int MAIN_EXPOSURE = 30;
 
 	private final int ARM_RES_X = 320;
 	private final int ARM_RES_Y = 240;
-	private final int ARM_FPS = 30;
+	private final int ARM_FPS = 15;
 	private final int ARM_EXPOSURE = 30;
 
 	public Cameras() {
@@ -24,7 +24,7 @@ public class Cameras {
 		mainCamera.setFPS(MAIN_FPS);
 		mainCamera.setExposureManual(MAIN_EXPOSURE);
 
-		armCamera = CameraServer.getInstance().startAutomaticCapture(0);
+		armCamera = CameraServer.getInstance().startAutomaticCapture(1);
 		armCamera.setResolution(ARM_RES_X, ARM_RES_Y);
 		armCamera.setFPS(ARM_FPS);
 		armCamera.setExposureManual(ARM_EXPOSURE);
