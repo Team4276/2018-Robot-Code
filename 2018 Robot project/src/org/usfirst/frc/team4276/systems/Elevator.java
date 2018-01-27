eb083f5f1f58487be32d189e737cb1a5065d8c5a
Austin Hwang <austin.y.hwang@gmail.com>
2018-01-27 15:09:18 -0800

    added manual override and update Telemetry to Elevator

--- 2018 Robot project/src/org/usfirst/frc/team4276/systems/Elevator.java
+++ 2018 Robot project/src/org/usfirst/frc/team4276/systems/Elevator.java
@@ -5,12 +5,14 @@ import org.usfirst.frc.team4276.utilities.Xbox;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Elevator {
 
-	TalonSRX elevatorDriver1, elevatorDriver2;
+	TalonSRX elevatorDriverMain;
+	VictorSPX elevatorDriverFollow;
 
 	final double POWER_OFFSET_MANIPULATOR = 4;
 	final double POWER_OFFSET_RAIL = 7;
@@ -23,22 +25,30 @@ public class Elevator {
 	final double SETPOINT_BOTTOM = 1;
 	final double SETPOINT_INCREMENT = .05;
 
+	private boolean manualOveride;
+	private boolean initializePID = true;
+	private double timeNow;
+	private double timePrevious;
+	private double timeStep;
+
 	double estimatedHeight = 0;
 	double commandedHeight = 0;
 	double heightError = 0;
 	double motorPower;
+	double assignedPower;
 
 	final double initiliazedPoint = 0;
 	final double derivativePoint = 0;
 
 	public Elevator(int elevator1CANPort, int elevator2CANPort) {
 
-		elevatorDriver1 = new TalonSRX(elevator1CANPort);
-		elevatorDriver2 = new TalonSRX(elevator2CANPort);
+		elevatorDriverMain = new TalonSRX(elevator1CANPort);
+		elevatorDriverFollow = new VictorSPX(elevator2CANPort);
 
-		elevatorDriver2.set(ControlMode.Follower, elevator2CANPort);
+		elevatorDriverFollow.set(ControlMode.Follower, elevator1CANPort);
 
 	}
+
 
 	public void performMainProcess() {
 
@@ -46,36 +56,60 @@ public class Elevator {
 		if (Robot.xboxController.getRawButton(Xbox.Y)) {
 			commandedHeight = SETPOINT_SCALE;
 		}
+
 		// button A(Deposit Cube in Switch)
 		if (Robot.xboxController.getRawButton(Xbox.A)) {
 			commandedHeight = SETPOINT_SWITCH;
 		}
+
 		// button B(Manipulator to Bottom)
 		if (Robot.xboxController.getRawButton(Xbox.B)) {
 			commandedHeight = SETPOINT_BOTTOM;
 		}
+
+/*		// Start + Right Axis Y (Manually control Height by Power)
+		if (Robot.xboxController.getRawButton(Xbox.Start)
+				&& (Math.abs(Robot.xboxController.getRawAxis(Xbox.RAxisY)) > -0.5)) {
+			manualOveride = true;
+		} else {
+			manualOveride = false;
+		}*/
+
+		// Right Axis Y (Manually Change Setpoint)
 		if (Robot.xboxController.getRawAxis(Xbox.RAxisY) > 0.5) {
 			commandedHeight = commandedHeight + SETPOINT_INCREMENT;
 		} else if (Robot.xboxController.getRawAxis(Xbox.RAxisY) < -0.5) {
 			commandedHeight = commandedHeight - SETPOINT_INCREMENT;
 		}
+
+		// Limit commanded height range
 		if (commandedHeight > MANIPULATOR_RAIL_HEIGHT) {
 			commandedHeight = MANIPULATOR_RAIL_HEIGHT;
 		} else if (commandedHeight > SETPOINT_BOTTOM) {
 			commandedHeight = SETPOINT_BOTTOM;
 		}
 
+		// PID control
 		if (commandedHeight < MANIPULATOR_RAIL_HEIGHT) {
-			estimatedHeight = elevatorDriver1.getSensorCollection().getQuadraturePosition();
+			// PID for manipulator rail
+			estimatedHeight = elevatorDriverMain.getSensorCollection().getQuadraturePosition();
 			heightError = commandedHeight - estimatedHeight;
 			motorPower = POWER_OFFSET_MANIPULATOR + (KP_MANIPULATOR * heightError);
-			elevatorDriver1.set(ControlMode.PercentOutput, motorPower);
+			elevatorDriverMain.set(ControlMode.PercentOutput, motorPower);
 		} else {
-			estimatedHeight = elevatorDriver1.getSensorCollection().getQuadraturePosition();
+			// PID for second rail
+			estimatedHeight = elevatorDriverMain.getSensorCollection().getQuadraturePosition();
 			heightError = commandedHeight - estimatedHeight;
 			motorPower = POWER_OFFSET_RAIL + (KP_RAIL * heightError);
-			elevatorDriver1.set(ControlMode.PercentOutput, motorPower);
+			elevatorDriverMain.set(ControlMode.PercentOutput, motorPower);
 		}
+/*
+		if (manualOveride == true) {
+			elevatorDriverMain.set(ControlMode.PercentOutput, Robot.xboxController.getRawAxis(Xbox.RAxisY));
+		}*/
+	}
+
+	public void updateTelemetry() {
 		SmartDashboard.putNumber("Commanded arm height", commandedHeight);
 		SmartDashboard.putNumber("Estimated arm height", estimatedHeight);
 	}
