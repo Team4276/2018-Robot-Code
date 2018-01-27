8fcf97b8b96794bebae2234c75a69fb43160dcc8
bistroboy <maxjaggli@gmail.com>
2018-01-27 13:46:15 -0800

    added updateTelemetry to Climber.java

--- 2018 Robot project/src/org/usfirst/frc/team4276/systems/Climber.java
+++ 2018 Robot project/src/org/usfirst/frc/team4276/systems/Climber.java
@@ -1,30 +1,39 @@
 package org.usfirst.frc.team4276.systems;
+
 import org.usfirst.frc.team4276.robot.Robot;
 import org.usfirst.frc.team4276.utilities.Xbox;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Climber {
 	VictorSPX climberMotor1, climberMotor2;
 	final double MOTOR_POWER = 1;
+	boolean climbInProgress = 0;
 
 	public Climber(int canPort1, int canPort2) {
 		climberMotor1 = new VictorSPX(canPort1);
 		climberMotor2 = new VictorSPX(canPort2);
-
 	}
 
 	public void performMainProcessing() {
 		if (Robot.xboxController.getPOV(Xbox.DPad) == Xbox.POVup) {
 			climberMotor1.set(ControlMode.PercentOutput, MOTOR_POWER);
 			climberMotor2.set(ControlMode.PercentOutput, MOTOR_POWER);
+			// when climber is on climbInProgress = true (bookkept)
+			climbInProgress = true;
 
 		} else {
 			climberMotor1.set(ControlMode.PercentOutput, 0);
 			climberMotor2.set(ControlMode.PercentOutput, 0);
+			// when climber is off climbInProgress = false (bookkept)
+			climbInProgress = false;
 		}
-
 	}
 
+	public void updateTelemetry() {
+		SmartDashboard.putBoolean("climb in progress", climbInProgress);
+	}
 }
